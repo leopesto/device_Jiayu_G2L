7fd96f80c4dc854bfa1bada9ea2f14c6e78331ac
leopesto <leopesto@gmail.com>
2012-12-03 23:06:21 +0100

    Initial Release

--- device_G2L.mk
+++ device_G2L.mk
@@ -17,6 +17,11 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += \
+    device/Jiayu/G2L/prebuilt/root/init.G2L.rc:root/init.G2L.rc \
+    device/Jiayu/G2L/prebuilt/root/init.G2L.usb.rc:root/init.G2L.usb.rc \
+    device/Jiayu/G2L/prebuilt/root/ueventd.G2L.rc:root/ueventd.rc \
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
